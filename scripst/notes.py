f94ec5fc7a2ffbcd00b64b0be69d8bfc3d8bb059
CARLOSCORDERO <carlos.alb.cordero@gmail.com>
2020-05-23 22:22:28 -0500

    [CCORDERO] papers and scrips udated

---
+++ scripst/notes.py
@@ -0,0 +1,13 @@
+#import datetime
+#datetime.date(year,month,day).weekday()
+
+# http://www.jaworski.ca/utmzones.htm
+# The aspect ratio of an image describes the proportional relationship between its width and its height. 
+
+#https://github.com/keras-team/keras-applications/blob/master/keras_applications/densenet.py
+
+#https://www.kaggle.com/kozistr/seedlings-densenet-161-48-public-lb-98-236
+
+# https://www.kaggle.com/xhlulu/densenet-keras#DenseNet-BC-161-48-no-top.h5
+
+# https://github.com/keras-team/keras-applications
